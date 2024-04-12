adfd17f74bc92996cfc6c84c47f36f2901b56868
Navneet <147032454+Navneet-Scaler@users.noreply.github.com>
2024-04-12 10:50:26 +0530

    Add files via upload

---
+++ fakeStrAPI_Spring_Bend/src/main/java/com/navneet/fakestoreapi_spring_bend/FakeStrApiSpringBendApplication.java
@@ -0,0 +1,13 @@
+package com.navneet.fakestoreapi_spring_bend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStrApiSpringBendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeStrApiSpringBendApplication.class, args);
+    }
+
+}
---
+++ fakeStrAPI_Spring_Bend/src/test/java/com/navneet/fakestoreapi_spring_bend/FakeStrApiSpringBendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navneet.fakestoreapi_spring_bend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStrApiSpringBendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
